f1ec3f054627218f8dbdf3b44634bf72526dfb2b
승민 (Eric Lee) <develretr0@gmail.com>
2026-06-20 07:47:54 +0000

    Vite 옵션 수정

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/wisket/',
+        scope: '/wisket/',
         icons: [
           {
             src: 'pwa-192x192.png',
